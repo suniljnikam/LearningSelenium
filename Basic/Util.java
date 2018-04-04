e65fa818311990ad105d5c5febe5d5ede5a8a541
suniljnikam <38042484+suniljnikam@users.noreply.github.com>
2018-04-04 15:35:38 +0530

    Create Util.java

---
+++ Basic/Util.java
@@ -0,0 +1,8 @@
+package AutomationFramework;
+
+public class Util {
+	public static final String FIRST_NAME = "Sunil";
+	public static final String LAST_NAME = "Nikam";
+	public static final String DRIVER_TYPE=	 "webdriver.chrome.driver";
+	public static final String DRIVER_PATH=	 "C:\\UBS\\Dev\\chromedriver.exe";
+}
