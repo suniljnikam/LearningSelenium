ae2783b360e5ea53ff0f5693f0889f066a2c795c
suniljnikam <38042484+suniljnikam@users.noreply.github.com>
2018-04-12 19:32:16 +0530

    Create Util.java

---
+++ Guru99BankProject/Util.java
@@ -0,0 +1,17 @@
+package AutomationFramework;
+
+public class Util {
+	public static final String FIRST_NAME = "Sunil";
+	public static final String LAST_NAME = "Nikam";
+	
+	public static final String USER_NAME = "mngr127275";
+	public static final String PASSWD = "ArybAse";
+	
+	public static final int WAIT_TIME=30;
+	
+	public static final String BASE_URL="http://demo.guru99.com/";
+	public static final String EXPECTED_TITLE = "Guru99 Bank Manager HomePage";
+	public static final String DRIVER_TYPE=	 "webdriver.chrome.driver";
+	public static final String DRIVER_PATH=	 "C:\\UBS\\Dev\\chromedriver.exe";
+	
+}
